7f62ec456a675474e78dc9a99479b82e6c2c706e
Trey Pendragon <tpendragon@princeton.edu>
2020-10-01 11:12:43 -0700

    Use location code as altrender on containers.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Princeton EAD Exporter
+
+Customizes EAD output to include location codes for containers.
---
+++ backend/model/princeton_ead_exporter.rb
@@ -0,0 +1,57 @@
+require 'nokogiri'
+require 'securerandom'
+
+class EADSerializer < ASpaceExport::Serializer
+  serializer_for :ead
+
+  def serialize_container(inst, xml, fragments)
+    atts = {}
+
+    sub = inst['sub_container']
+    top = sub['top_container']['_resolved']
+
+    atts[:id] = prefix_id(SecureRandom.hex)
+    last_id = atts[:id]
+
+    atts[:type] = top['type']
+    text = top['indicator']
+
+    atts[:label] = I18n.t("enumerations.instance_instance_type.#{inst['instance_type']}",
+                          :default => inst['instance_type'])
+    atts[:label] << " [#{top['barcode']}]" if top['barcode']
+
+    # Add location code to the altrender attribute.
+    if(collection = top["collection"])
+      if(collection && collection.first)
+        collection_identifier = collection.first["identifier"]
+        if (location_code = top["long_display_string"].match(/\[([^\[\]]*)\], #{collection_identifier}/))
+          atts[:altrender] = location_code[1]
+        end
+      end
+    end
+    # if (cp = top['container_profile'])
+    #   atts[:altrender] = cp['_resolved']['url'] || cp['_resolved']['name']
+    # end
+
+    xml.container(atts) {
+      sanitize_mixed_content(text, xml, fragments)
+    }
+
+    (2..3).each do |n|
+      atts = {}
+
+      next unless sub["type_#{n}"]
+
+      atts[:id] = prefix_id(SecureRandom.hex)
+      atts[:parent] = last_id
+      last_id = atts[:id]
+
+      atts[:type] = sub["type_#{n}"]
+      text = sub["indicator_#{n}"]
+
+      xml.container(atts) {
+        sanitize_mixed_content(text, xml, fragments)
+      }
+    end
+  end
+end
